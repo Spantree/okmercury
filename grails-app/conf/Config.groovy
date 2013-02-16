1e583983d58717978832eaa2af3e9ed6fa420d32
Cedric G Hurst II <cedric@spantree.net>
2013-02-16 07:35:09 -0600

    setting root log level to info

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -78,6 +78,10 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
+	
+	root {
+		info()
+	}
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
