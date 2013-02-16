6d2499e6a2dd156faff50767e0d5d476aba23040
Gary Turovsky <flyhighplato@gmail.com>
2013-02-16 14:06:03 -0600

    Adding AnswerMatch and UserMatch domain classes #23 #24

---
+++ grails-app/domain/co/okmercury/AnswerMatch.groovy
@@ -0,0 +1,10 @@
+package co.okmercury
+
+class AnswerMatch {
+	User userA
+	User userB
+	Integer scoreForUserA
+	Integer pointsPossibleForUserA
+	Integer scoreForUserB
+	Integer pointsPossibleForUserB
+}
---
+++ grails-app/domain/co/okmercury/UserMatch.groovy
@@ -0,0 +1,18 @@
+package co.okmercury
+
+class UserMatch {
+	User principalUser
+	User matchUser
+	
+	Integer principalPoints
+	Integer principalPointsPossible
+	
+	Integer matchPoints
+	Integer matchPointsPossible
+	
+	Float principalPercentageScore
+	Float matchPercentageScore
+	
+	Float overallScore
+
+}
