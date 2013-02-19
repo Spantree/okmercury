abaf8a076b63e148bc33c8f750ab262ce7e5b4da
Cedric G Hurst II <cedric@spantree.net>
2013-02-19 02:24:49 -0600

    moving bootstrap logic to reset

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,31 +1,22 @@
 import co.okmercury.User
+import co.okmercury.UserService
 
-class BootStrap {
+class userService {
+	def grailsApplication
 
     def init = { servletContext ->
-		[
-			[firstName: "Cedric", lastName: "Hurst", email: "cedric", gravatarHash: "22c2f4465298a422b5307c0ec5a48442"],
-			[firstName: "Gary", lastName: "Turovsky", email: "gary", gravatarHash: "0bdee349222a510f27abd7ebe84c6e00"],
-			[firstName: "Jane", lastName: "Smith", email: "jane", gravatarHash: "c87a84cf3b06077036deade1b44aa45e"],
-			[firstName: "John", lastName: "Doe", email: "john", gravatarHash: "25c7c18223fb42a4c6ae1c8db6f50f9b"],
-			[firstName: "Paul", lastName: "Barry", email: "paul", gravatarHash: "6661ef9d747db3af8896cd94959d717d"],
-			[firstName: "Kim", lastName: "Bekkelund", email: "kim", gravatarHash: "6c51c14716e24bc1f1a3fb5ad234e773"],
-			[firstName: "Rebecca", lastName: "Murphey", email: "rebecca", gravatarHash: "0177cdce6af15e10db15b6bf5dc4e0b0"]
-		].each { Map props ->
-			User user = User.findByEmail(props.email)
-			if(!user) {
-				user = new User(email: props.email)
-			}
-			user.firstName = props.firstName
-			user.lastName = props.lastName
-			user.passwordHash = "password"
-			user.gravatarHash = props.gravatarHash
-			user.save(flush: true)
-			if(user.errors.hasErrors()) {
-				log.error "Error occurred saving user ${props.email}: ${user.errors.allErrors}"
-			} else {
-				log.info "Successfully updated user ${props.email}"
-			}
+		User user = User.findByEmail('admin@okmercury.co')
+		if(!user) {
+			log.info "Creating admin user"
+			UserService userService = grailsApplication.mainContext.getBean('userService')
+			userService.createUser(
+				'admin@okmercury.co',
+				'password',
+				'Administrator',
+				'of OKMercury',
+				'Administrator',
+				'OKMercury'
+			)
 		}
     }
     def destroy = {
--- grails-app/controllers/co/okmercury/ResetController.groovy
+++ grails-app/controllers/co/okmercury/ResetController.groovy
@@ -1,15 +1,20 @@
 package co.okmercury
 
+import org.apache.commons.lang.math.RandomUtils
 import org.bson.types.ObjectId
 
 import com.mongodb.BasicDBObject
 import com.mongodb.DB
 import com.mongodb.DBObject
+import com.mongodb.WriteConcern
 
 class ResetController {
+	UserService userService
 	QuestionService questionService
+	MatchService matchService
+	AnswerService answerService
 	def mongo
-	
+
 	def questions = [
 		[
 			question: "I work for...",
@@ -80,24 +85,110 @@ class ResetController {
 			]
 		]
 	]
-	
-	def reset() {
-		User user = User.findByEmail('cedric')
-		
+
+	def userMaps = [
+		[
+			firstName: "Cedric", lastName: "Hurst", email: "cedric@spantree.net",
+			companyName: "Spantree Technology Group, LLC", jobTitle: "Principal & Lead Software Architect"
+		],
+		[
+			firstName: "Gary", lastName: "Turovsky", email: "gary@spantree.net",
+			companyName: "Spantree Technology Group, LLC", jobTitle: "Senior Software Engineer"
+		],
+		[
+			firstName: "Hilary", lastName: "Freemason", email: "hmason@notarealemail.com",
+			companyName: "Bitly", jobTitle: "Chief Scientist", gravatarHash: "c87a84cf3b06077036deade1b44aa45e"	
+		],
+		[
+			firstName: "Tom", lastName: "Preston", email: "mojombo@notarealemail.com",
+			companyName: "Extreme Events", jobTitle: "Cofounder", gravatarHash: "25c7c18223fb42a4c6ae1c8db6f50f9b"	
+		],
+		[
+			firstName: "Rebecca", lastName: "Murray", email: "rmurphey@notarealemail.com",
+			companyName: "New Orleans Merchants Association", jobTitle: "Director", gravatarHash: "0177cdce6af15e10db15b6bf5dc4e0b0"	
+		]
+	]
+
+	def deleteStuff() {
 		log.info "Deleting all the things!"
 		DB db = mongo.getDB('okmercury')
 		DBObject allQuery = [:] as BasicDBObject
-		db.userMatch.remove(allQuery)
-		db.questionMatch.remove(allQuery)
-		db.answer.remove(allQuery)
-		db.questionOption.remove(allQuery)
-		db.question.remove(allQuery)
-		
+		db.userMatch.remove(allQuery, WriteConcern.SAFE)
+		db.questionMatch.remove(allQuery, WriteConcern.SAFE)
+		db.answer.remove(allQuery, WriteConcern.SAFE)
+		db.questionOption.remove(allQuery, WriteConcern.SAFE)
+		db.question.remove(allQuery, WriteConcern.SAFE)
+		db.user.remove(allQuery, WriteConcern.SAFE)
+	}
+	
+	def addUsers() {
+		userMaps.collect { Map map ->
+			userService.createUser(
+				map.email,
+				'password',
+				map.firstName,
+				map.lastName,
+				map.jobTitle,
+				map.companyName,
+				map.gravatarHash
+			)
+		}
+	}
+	
+	def addQuestions() {
+		List<User> users = User.list()
+		Random r = new Random()
 		questions.each { Map map ->
+			Collections.shuffle(users, r)
 			ObjectId id = new ObjectId()
 			log.info "Adding question: '${map.question}' as ${id}"
-			questionService.updateQuestion(id.toString(), map.question, map.options, user)
+			questionService.updateQuestion(id.toString(), map.question, map.options, users[0])
 		}
+	}
+	
+	def addFakeAnswers() {
+		List<User> users = User.list()
+		List<Question> questions = Question.list()
+		List<Importance> importances = Importance.class.getEnumConstants()
+		Random r = new Random()
+		questions.each { Question question ->
+			List<QuestionOption> options = QuestionOption.where { question == question }.list().findAll {
+				it.answer != "I am currently unemployed"
+			}
+			users.each { User user ->
+				boolean shouldSkip = r.nextInt() % 10 == 0
+				if(shouldSkip) {
+					answerService.saveAnswerSkipped(user, question.id.toString(), "")
+				} else {
+					Collections.shuffle(options, r)
+					Collections.shuffle(importances, r)
+					QuestionOption userAnswer = options[0]
+	
+					Collections.shuffle(options, r)
+					Importance importance = importances[0]
+					List<String> acceptableAnswerIds = []
+					if(importance != Importance.IRRELEVANT) {
+						int numberAcceptable = RandomUtils.nextInt(r, options.size()-1)+1
+						acceptableAnswerIds = options[1..numberAcceptable].collect { it.id.toString() }
+					}
+					answerService.saveAnswer(
+						user,
+						question.id.toString(),
+						userAnswer.id.toString(),
+						acceptableAnswerIds,
+						importance.name(),
+						""
+					)
+				}
+			}
+		}
+	}
+	
+	def reset() {
+		deleteStuff()
+		addUsers()
+		addQuestions()
+		addFakeAnswers()
 		[:]
 	}
 }
